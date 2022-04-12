b8da3f5e5d6b8c1c412fecdcf969c7f1ca24651d
Rajeshrj67 <103481347+Rajeshrj67@users.noreply.github.com>
2022-04-12 16:50:52 +0530

    Update ClassC.java

--- com.crm.comcast.SDET32_GitPractice/src/test/java/com/crm/comcast/SDET32_GitPractice/ClassC.java
+++ com.crm.comcast.SDET32_GitPractice/src/test/java/com/crm/comcast/SDET32_GitPractice/ClassC.java
@@ -6,8 +6,12 @@ public class ClassC {
 
 	@Test
 	public void demoTest3() {
-		System.out.println("push 3");
-		System.out.println("push 3");
+	System.out.println("push 3");
+		System.out.println("push 4");
+		System.out.println("push 5");
+		System.out.println("push 6");
+		System.out.println("push 7");
+		System.out.println("push 8");
 
 	}
 }
