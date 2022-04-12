3df6932444018e2da382c344c4a35f50f67efe21
Rajesh RJ <Rajesh RJ@RAJESH>
2022-04-12 12:52:09 +0530

    commit three

---
+++ com.crm.comcast.SDET32_GitPractice/src/test/java/com/crm/comcast/SDET32_GitPractice/ClassC.java
@@ -0,0 +1,11 @@
+package com.crm.comcast.SDET32_GitPractice;
+
+import org.testng.annotations.Test;
+
+public class ClassC {
+
+	@Test
+	public void demoTest3() {
+		System.out.println("push 3");
+	}
+}
